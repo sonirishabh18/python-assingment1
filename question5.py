1cf298700c68ccdc057e97ec5f6fa77a32bfd0e7
sonirishabh18 <sonyrishabh18@gmail.com>
2026-06-03 00:49:16 +0530

    Create question5.py

---
+++ question5.py
@@ -0,0 +1,6 @@
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+print("Complete List:", fruits)
+print("First Fruit:", fruits[0])
+print("Last Fruit:", fruits[-1])
+print("Total Items:", len(fruits))
